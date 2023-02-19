38eaebe68494a9d1bb2274e79fac13596ec67f27
AliceMonte <lol.gerda96@gmail.com>
2023-02-19 11:46:56 +0300

    django 93-109

---
+++ vebsite/main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Task
+
+def index(request):
+    task = Task.objects.all()
+    print(task)
+    return render(request, 'main/index.html', {'title': 'Главная страница', 'tasks': 'tasks'})
+
+def about(request):
+    return render(request, 'main/about.html')
+
