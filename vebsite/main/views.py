e860f5b92e500d66f9e59b129ea6cedc81da90f9
AliceMonte <lol.gerda96@gmail.com>
2023-02-20 20:12:05 +0300

    lesson 104

--- vebsite/main/views.py
+++ vebsite/main/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Task
+from .forms import TaskForm
 
 def index(request):
-    task = Task.objects.all()
-    print(task)
-    return render(request, 'main/index.html', {'title': 'Главная страница', 'tasks': 'tasks'})
+    tasks = Task.objects.order_by('id')
+    return render(request, 'main/index.html', {'title': 'Главная страница', 'tasks': tasks})
 
 def about(request):
     return render(request, 'main/about.html')
-
+def create(request):
+    form = TaskForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'main/create.html')
